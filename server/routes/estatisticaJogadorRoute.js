5ac67a4a9ab5687fbe5b738ac11333a9709361d1
VLBortolotti <vitorlacobortolotti@gmail.com>
2024-04-04 01:52:15 -0300

    Fix: getAllEstatisticas route was not being reached in the endpoints stack

--- server/routes/estatisticaJogadorRoute.js
+++ server/routes/estatisticaJogadorRoute.js
@@ -10,8 +10,8 @@ router.patch('/jogador', estatisticaJogadorController.getEstatisticaJogadorById)
 router.get('/campeonato/:id', estatisticaJogadorController.getEstatisticaJogadorByCampeonatoId)
 router.get('/jogo/:id', estatisticaJogadorController.getEstatisticaJogoById)
 router.get('/punidos', estatisticaJogadorController.getAllEstatisticasJogadoresPunidos)
+router.get('/all', estatisticaJogadorController.getAllEstatisticas)
 router.get('/:id', estatisticaJogadorController.getAllEstatisticaByJogadorId)
-router.get('/', estatisticaJogadorController.getAllEstatisticas)
 
 router.patch('/:id', estatisticaJogadorController.updateEstatisticaById)
 
