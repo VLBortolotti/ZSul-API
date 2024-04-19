140b9ebefcd6dc37e79155c12f30af8332f848bf
VLBortolotti <vitorlacobortolotti@gmail.com>
2024-04-19 01:33:09 -0300

    Update: exportSumulaByTeamAndCampeonatoId now sends a better response in case theres an error

--- server/controllers/sumulaController.js
+++ server/controllers/sumulaController.js
@@ -57,7 +57,7 @@ exports.exportSumulaByTeamAndCampeonatoId = async (req, res, next) => {
     const response = await sumulaService.exportSumulaByTeamAndCampeonatoId(teamId, campeonatoId)
 
     if (response.message !== 'ok') {
-        res.status(response.status).json({ status: response.status, msg: response.message })
+        res.status(response.status).json({ status: response.status, msg: response.message, erro: response.message, data: null })
     }
     
     const filePath = path.resolve(__dirname, '..', response.data)
